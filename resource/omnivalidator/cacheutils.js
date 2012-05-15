2dc9fd55ce154fd82e513e7b0032da3a6477031e
Kevin Locke <kevin@kevinlocke.name>
2012-05-15 08:44:40 -0600

    Add deinhibitCaching function to cacheutils
    This function adds an observer to the http-modify-request topic which
    will remove the INHIBIT_CACHING flag from the loadFlags for document
    requests for top-level windows.  This is necessary for the content to be
    cached to allow us to validate the content using the cached version.
    
    Note:  This function is currently unused, as I have not found a case
    where INHIBIT_CACHING is set on document requests for top-level windows.
    However, it represents a significant amount of work and there may still
    be undiscovered cases where this occurs, so it is being committed in
    case it may be useful later.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- resource/omnivalidator/cacheutils.js
+++ resource/omnivalidator/cacheutils.js
@@ -19,7 +19,131 @@ define(
     function (Cc, Ci, log4moz, CacheID) {
         "use strict";
 
-        var logger = log4moz.repository.getLogger("omnivalidator.cacheutils");
+        var logger = log4moz.repository.getLogger("omnivalidator.cacheutils"),
+            observingRequests = false,
+            requestObserver;
+
+        requestObserver = {
+            observe: function (subject, topic, data) {
+                var channel, loadContext, loadFlags, reqWin;
+
+                // Important:  This happens on every request, keep it fast!
+
+                // Note:  If caching is made conditional or only enabled
+                // for windows in which Omnivalidator is enabled, see
+                // http://stackoverflow.com/questions/6484823/finding-the-tab-associated-with-a-dom-window/6489272#6489272
+                // for one rather ugly way of finding the browser window
+
+                try {
+                    channel = subject.QueryInterface(Ci.nsIChannel);
+                } catch (ex) {
+                    // Not an nsIChannel, don't need to cache it
+                    return;
+                }
+
+                // https://developer.mozilla.org/en/Updating_extensions_for_Firefox_3.5#Getting_a_load_context_from_a_request
+                try {
+                    loadContext = channel.notificationCallbacks
+                        .getInterface(Ci.nsILoadContext);
+                } catch (ex2) {
+                    try {
+                        loadContext = channel.loadGroup.notificationCallbacks
+                            .getInterface(Ci.nsILoadContext);
+                    } catch (ex3) {
+                        // No loadContext associated with the request
+                        return;
+                    }
+                }
+
+                loadFlags = channel.loadFlags;
+                /*jslint bitwise: true */
+                if (!(loadFlags & Ci.nsIChannel.LOAD_DOCUMENT_URI)) {
+                    // Not a document
+                    /* Don't log in production for performance reasons
+                    logger.debug("Request to " + channel.URI.spec + " (" +
+                            loadFlags.toString(16) +
+                            ") does not have LOAD_DOCUMENT_URI (" +
+                            Ci.nsIChannel.LOAD_DOCUMENT_URI.toString(16) +
+                            ") set");
+                    */
+                    return;
+                }
+                /*jslint bitwise: false */
+
+                reqWin = loadContext.associatedWindow;
+                if (!reqWin || reqWin !== reqWin.top) {
+                    // Window is not the top window
+                    /* Don't log in production for performance reasons
+                    logger.debug(reqWin + " is not the top window for " + channel.URI.spec);
+                    */
+                    return;
+                }
+
+                // Remove the INHIBIT_CACHING flag
+                // If INHIBIT_CACHING is set, replace it with
+                // INHIBIT_PERSISTENT_CACHING since the only caching we need
+                // is memory caching.
+                /*jslint bitwise: true */
+                if (loadFlags & Ci.nsIRequest.INHIBIT_CACHING) {
+                    channel.loadFlags =
+                        (loadFlags & ~Ci.nsIRequest.INHIBIT_CACHING)
+                            | Ci.nsIRequest.INHIBIT_PERSISTENT_CACHING;
+                    /* Don't log in production for performance reasons
+                    logger.trace("Removed INHIBIT_CACHING flag on request to " +
+                        channel.URI.spec);
+                    */
+                } else {
+                    // INHIBIT_CACHING not present
+                    /* Don't log in production for performance reasons
+                    logger.error("Request to " + channel.URI.spec + " (" +
+                            loadFlags.toString(16) +
+                            ") does not have INHIBIT_CACHING (" +
+                            Ci.nsIRequest.INHIBIT_CACHING.toString(16) +
+                            ") set");
+                    */
+                }
+                /*jslint bitwise: false */
+            }
+        };
+
+        /* Note:  This function is not currently used, as I have not found a
+         * case where INHIBIT_CACHING is set on a request we care about.
+         * If such a case is discovered, it may be called from global.js
+         * as part of the extension setup.
+         */
+        function deinhibitCaching(deinhibit) {
+            var observerService;
+
+            logger.debug((deinhibit ? "Deinhibiting" : "Not deinhibiting") +
+                " in-memory caching of document requests for top-level windows");
+
+            observerService = Cc["@mozilla.org/observer-service;1"]
+                .getService(Ci.nsIObserverService);
+
+            if (arguments.length === 0 || deinhibit) {
+                if (!observingRequests) {
+                    logger.debug("Adding request observer to deinhibit " +
+                        "in-memory caching of document requests for top-level windows");
+                    observerService.addObserver(
+                        requestObserver,
+                        "http-on-modify-request",
+                        false
+                    );
+                    observingRequests = true;
+                }
+            } else {
+                if (observingRequests) {
+                    logger.debug("Removing request observer which will " +
+                        "allow requests with inhibited-caching.");
+                    observerService.removeObserver(
+                        requestObserver,
+                        "http-on-modify-request",
+                        false
+                    );
+                    observingRequests = false;
+                }
+            }
+        }
 
         function getDocumentCacheKey(doc) {
             var docWin, pageDescriptor;
@@ -75,6 +199,7 @@ define(
         }
 
         return {
+            deinhibitCaching: deinhibitCaching,
             getChannel: getChannel,
             getDocumentCacheKey: getDocumentCacheKey,
             getDocumentChannel: getDocumentChannel
