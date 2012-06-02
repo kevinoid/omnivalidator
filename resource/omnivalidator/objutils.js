2bcad644d6638edbe97e98a19e1a18aac555f563
Kevin Locke <kevin@kevinlocke.name>
2012-06-02 16:09:32 -0600

    The return of objutils
    For need of (partial) Object.create functionality on ES3, objutils is
    being resurrected with a create function which either aliases
    Object.create or provides a partial implementation for ES3.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

---
+++ resource/omnivalidator/objutils.js
@@ -0,0 +1,34 @@
+/* Utility functions for dealing with objects.
+ *
+ * This file is part of the Omnivalidator extension for Firefox.
+ * It is licensed under the terms of the MIT License.
+ * The complete text of the license is available in the project documentation.
+ *
+ * Copyright 2012 Kevin Locke <kevin@kevinlocke.name>
+ */
+/*jslint indent: 4, plusplus: true, unparam: true */
+/*global define */
+
+define(
+    [
+    ],
+    function () {
+        "use strict";
+
+        /** Object.create for ES3
+         *
+         * No support for the properties object.
+         */
+        function createES3(proto) {
+            function Constructor() {}
+            Constructor.prototype = proto;
+            return new Constructor();
+        }
+
+        return {
+            create: Object.create || createES3
+        };
+    }
+);
+
+// vi: set sts=4 sw=4 et :
