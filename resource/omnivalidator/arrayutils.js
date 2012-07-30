5f204f5b146c5f912092d822dce421aeaa45bb47
Kevin Locke <kevin@kevinlocke.name>
2012-07-30 13:25:28 -0600

    Add merge and mergeUniq to arrayutils
    More utility functions that may come in handy when dealing with sorted
    arrays.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- resource/omnivalidator/arrayutils.js
+++ resource/omnivalidator/arrayutils.js
@@ -15,6 +15,65 @@ define(
     function () {
         "use strict";
 
+        /** Merges two sorted arrays.
+         * @returns a sorted array containing a1 and a2.
+         */
+        function merge(a, b) {
+            var c = [], i = 0, j = 0;
+            while (i < a.length && j < b.length) {
+                if (a[i] < b[j]) {
+                    c.push(a[i++]);
+                } else {
+                    c.push(b[j++]);
+                }
+            }
+
+            while (i < a.length) {
+                c.push(a[i++]);
+            }
+            while (j < b.length) {
+                c.push(b[j++]);
+            }
+
+            return c;
+        }
+
+        function mergeUniq(a, b) {
+            var c = [], i = 0, j = 0, last = {};
+            while (i < a.length && j < b.length) {
+                if (a[i] < b[j]) {
+                    if (a[i] !== last) {
+                        last = a[i];
+                        c.push(last);
+                    }
+                    ++i;
+                } else {
+                    if (b[j] !== last) {
+                        last = b[j];
+                        c.push(last);
+                    }
+                    ++j;
+                }
+            }
+
+            while (i < a.length) {
+                if (a[i] !== last) {
+                    last = a[i];
+                    c.push(last);
+                }
+                ++i;
+            }
+            while (j < b.length) {
+                if (b[j] !== last) {
+                    last = b[j];
+                    c.push(last);
+                }
+                ++j;
+            }
+
+            return c;
+        }
+
         /** Find the lowest index at which an object may be inserted which
          * maintains sorted order using binary search.
          *
@@ -42,6 +101,8 @@ define(
         }
 
         return {
+            merge: merge,
+            mergeUniq: mergeUniq,
             sortedIndex: sortedIndex
         };
     }
